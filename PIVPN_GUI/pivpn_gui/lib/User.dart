44e61eb9fa06e8be02060a6845aaea75303bd238
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2025-01-03 18:13:05 +0100

    Finished class

--- PIVPN_GUI/pivpn_gui/lib/User.dart
+++ PIVPN_GUI/pivpn_gui/lib/User.dart
@@ -12,6 +12,9 @@ class User {
   // Constructor
   User(this.name, this.subName, this.startDate, this.endDate, this.isDisabled, this.isAlwaysAllowed);
 
-  
+  // Methods
+  String toString(){
+    return "Name: ${this.name} Sub Name: ${this.subName} Start Date: ${this.startDate} End Date ${this.endDate} Is Disabled? ${this.isDisabled} Is Always Allowed? ${this.isAlwaysAllowed}";
+  }
    
 }
