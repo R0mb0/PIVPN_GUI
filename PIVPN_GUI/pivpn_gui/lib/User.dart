83a74188d3382e02583b25d59307d6eb34f04f1b
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2025-01-03 17:03:54 +0100

    made progress

---
+++ PIVPN_GUI/pivpn_gui/lib/User.dart
@@ -0,0 +1,17 @@
+class User {
+
+  // Fields 
+  // Using var type for consistency 
+  var name; 
+  var subName; 
+  var startDate; 
+  var endDate; 
+  var isDisabled;
+  var isAlwaysAllowed; 
+
+  // Constructor
+  User(this.name, this.subName, this.startDate, this.endDate, this.isDisabled, this.isAlwaysAllowed);
+
+  
+   
+}
