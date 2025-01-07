366f08a11f2dd84adcd1b43709def3bf37276f84
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2025-01-07 09:56:07 +0100

    Only created a new class

---
+++ PIVPN_GUI/pivpn_gui/lib/CLI_Adapater.dart
@@ -0,0 +1,35 @@
+class CliAdapater 
+{
+  
+
+  // Methods
+
+  // Add user 
+  void AddUser(String name){
+
+  }
+
+  // Remove user
+  void RemoveUser(String name)
+  {
+    
+  }
+
+  // Get All Users
+  void GetAllUsers()
+  {
+
+  }
+
+  // Enable user 
+  void EnableUser(String name)
+  {
+
+  }
+
+  // Disable user 
+  void DisableUser(String name)
+  {
+
+  }
+}
